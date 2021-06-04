3a58dc66a67e5c5a156cfe020b47822f01032902
raghav94603 <34892373+raghav94603@users.noreply.github.com>
2021-06-04 20:35:45 +0530

    Update activity.java

--- Activities/activity.java
+++ Activities/activity.java
@@ -1,40 +1,56 @@
 import java.util.*;
 public class activity {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	Scanner s=new Scanner(System.in);
-	int n,i,count=0;	
-	int[] array=new int[20];
-	System.out.println("Enter The Array Size : ");
-	n=s.nextInt();
-	
-	System.out.println("Enter The Array Elements : \n");
-	for(i=0;i<n;i++)
+	public void findElementcount(int array[],int n,int num)
 	{
-		array[i]=s.nextInt();
-		if(array[i]<0)
+		int i,count=0;
+		
+		if(num<0)
 		{
 			System.out.println("Invalid Input");
 			System.exit(0);
 		}
-	}
-	System.out.println("Enter The Number To find : ");
-	int num=s.nextInt();
-	if(num<0)
-	{
-		System.out.println("Invalid Input");
-		System.exit(0);
-	}
-	for(i=0;i<n;i++)
-	{
-		if(array[i]==num)
+		for(i=0;i<n;i++)
 		{
-			count++;
+			if(array[i]==num)
+			{
+				count++;
+			}
 		}
+		System.out.println("The Number "+num+" occurs"+" "+count+" "+"Times");
+		
+		
 	}
-	System.out.println("The Number "+num+" occurs "+count+" Times");
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		
+		int array[]=new int[20],n,num,i=0;
+		
+		System.out.println("Enter The Array Size : ");
+		n=s.nextInt();
+		
+		System.out.println("Enter The Array Elements : \n");
+		for(i=0;i<n;i++)
+		{
+			array[i]=s.nextInt();
+		
+		
+			if(array[i]<0)
+			{
+				System.out.println("Invalid Input");
+				System.exit(0);
+			}
+		}
+		
+		
+		System.out.println("Enter The Number To find : ");
+		 num=s.nextInt();
+		
+		activity obj=new activity();
+		obj.findElementcount(array, n,num);
+		
 	
 	
 	
